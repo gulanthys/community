00adee2e45d823b8a7825f9cb7b93e5f7b290800
25656 <2565640028@qq.com>
2023-10-19 16:38:32 +0800

    websocket初始化

--- community-friends/src/main/java/com/yanty/friends/config/WebsocketConfig.java
+++ community-friends/src/main/java/com/yanty/friends/config/WebsocketConfig.java
@@ -1,12 +1,21 @@
 package com.yanty.friends.config;
 
-
+import com.yanty.friends.ws.DefaultWebSocketHandler;
+import com.yanty.friends.ws.WebSocketInterceptor;
+import com.yanty.friends.ws.service.WebSocket;
+import com.yanty.friends.ws.service.impl.WebSocketImpl;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
-public class WebsocketConfig {
+@ConditionalOnProperty(name = "spring.profiles.active", havingValue = "dev")
+public class WebsocketConfig{
 
     @Bean
     //ServerEndpointExporter，自动注册使用@ServerEndpoint
@@ -14,5 +23,30 @@ public class WebsocketConfig {
         return new ServerEndpointExporter();
     }
 
+//    @Bean
+//    public DefaultWebSocketHandler defaultWebSocketHandler() {
+//        return new DefaultWebSocketHandler();
+//    }
+//
+//    @Bean
+//    public WebSocket webSocket() {
+//        return new WebSocketImpl();
+//    }
+//
+//    @Bean
+//    public WebSocketInterceptor webSocketInterceptor() {
+//        return new WebSocketInterceptor();
+//    }
+
+//    /**
+//     * 接口注册
+//     * @param registry
+//     */
+//    @Override
+//    public void registerWebSocketHandlers(@NonNull WebSocketHandlerRegistry registry) {
+//        registry.addHandler(defaultWebSocketHandler(), "ws/message")
+//                .addInterceptors(webSocketInterceptor())
+//                .setAllowedOrigins("*");
+//    }
 
 }
