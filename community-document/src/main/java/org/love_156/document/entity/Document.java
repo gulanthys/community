7da98851308a200654270b6f9d3f3ff2d43e95f9
xxk <love_156648@163.com>
2023-10-19 21:45:13 +0800

    2023.10.19 xxk

--- community-document/src/main/java/org/love_156/document/entity/Document.java
+++ community-document/src/main/java/org/love_156/document/entity/Document.java
@@ -3,10 +3,10 @@ package org.love_156.document.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.w3c.dom.Text;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -21,24 +21,26 @@ public class Document implements Serializable {
      * 作者Id
      */
     @TableId(type = IdType.AUTO)
-    public Integer AuthorID;
+    private Integer AuthorID;
 
     /**
      * 标题
      */
-    public String Title;
+    private String Title;
 
     /**
      * 最后编辑时间
      */
-    public Date EditDate;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private Date EditDate;
     /**
      * 创建时间
      */
-    public Date CreatDate;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private Date CreatDate;
 
     /**
      * 文章内容
      */
-    public  String Text;
+    private String Text;
 }
