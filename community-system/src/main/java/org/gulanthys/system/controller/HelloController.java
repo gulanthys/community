663ae8af6ebcb4976e5bccfc193b7b9af0f5542b
周宇杰 <1348562017@qq.com>
2023-11-01 16:46:14 +0800

    11/1 Gateway

--- community-system/src/main/java/org/gulanthys/system/controller/HelloController.java
+++ community-system/src/main/java/org/gulanthys/system/controller/HelloController.java
@@ -2,13 +2,13 @@ package org.gulanthys.system.controller;
 
 import org.community.common.Constants;
 import org.community.common.Result;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class HelloController {
-    @PostMapping("/hello")
+    @GetMapping("/hello")
     public Result<?> Hello() {
         String hello = "hello";
         return Result.buildResult(Constants.Status.OK, hello, hello);
